768bb613cea1b0ea10e1e6e33e7277bf20a549c5
EVGENIY BARSUKOV <evgeniy.barsukov@simbirsoft.com>
2024-08-11 23:20:20 +0400

    k

--- pr_121.py
+++ pr_121.py
@@ -1,7 +1,19 @@
+from typing import List
+
 p = [7,1,5,3,6,4]
 
 
-def maxProfit(prices):
-    
+def maxProfit(prices: List[int]) -> int:
+    buy = prices[0]
+    profit = 0
+    for price in prices[1:]:
+        sell = price
+        real_profit = sell - buy
+        if real_profit > profit:
+            profit = real_profit
+        if price < buy:
+            buy = price
+    return profit
+
 
 print(maxProfit(p))
